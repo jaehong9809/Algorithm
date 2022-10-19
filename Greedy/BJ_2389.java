445526f18f72c17cd4ae51b0c9cd28857ee590f3
LeeJaeHong <0915jhlee@daum.net>
2022-10-19 14:55:05 +0900

    solve greedy

--- Greedy/BJ_2389.java
+++ Greedy/BJ_2389.java
@@ -1,6 +1,8 @@
+package Greedy;
+
 import java.util.Scanner;
 
-public class Main {
+public class BJ_2389 {
     static int n;
     public static void main(String[] args) {
         Scanner sc= new Scanner(System.in);
