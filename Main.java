60a5c61bf61d5ff00ce43f015e243facdfd85365
LeeJaeHong <0915jhlee@daum.net>
2022-10-19 15:05:58 +0900

    solve greedy

--- Main.java
+++ Main.java
@@ -1,26 +1,9 @@
-import java.util.Scanner;
+
 
 public class Main {
-    static int n;
+
     public static void main(String[] args) {
-        Scanner sc= new Scanner(System.in);
-        n= sc.nextInt();
-        int result=0;
-        while(n>0){
-            if(n%5==0){
-                n-=5;
-                result++;
-            }
-            else{
-                n-=3;
-                if(n<0){
-                    result=-1;
-                    break;
-                }
-                result++;
-            }
-        }
-        System.out.println(result);
+
     }
 
 }
