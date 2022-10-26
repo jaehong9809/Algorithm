a8ebcd499b44503e7e632ab477c37075b0b5441f
LeeJaeHong <0915jhlee@daum.net>
2022-10-27 00:00:26 +0900

    plus set structure!

--- DataStructures/BJ_14425.java
+++ DataStructures/BJ_14425.java
@@ -1,3 +1,5 @@
+package DataStructures;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -5,7 +7,7 @@ import java.util.HashSet;
 import java.util.Set;
 import java.util.StringTokenizer;
 
-class Main{
+class BJ_14425 {
     static Set<String> set = new HashSet<>();
     static int n, m;
     public static void main(String[] args) throws IOException {
