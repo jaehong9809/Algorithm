ccfefaccc292bfa6784e86479c37c4ba53fc4033
LeeJaeHong <0915jhlee@daum.net>
2022-10-31 00:01:18 +0900

    add bfs

--- Graph/BJ_18352.java
+++ Graph/BJ_18352.java
@@ -1,9 +1,11 @@
+package Graph;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.*;
 
-class Main{
+class BJ_18352 {
     static ArrayList<ArrayList<Integer>> arrayList=new ArrayList<>();
     static int n, m, k, start;
     public static void main(String[] args) throws IOException {
