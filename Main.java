54c454752fa1c1b25deb43c45b12865c70984cde
LeeJaeHong <0915jhlee@daum.net>
2022-11-02 00:00:40 +0900

    hard graph clear

--- Main.java
+++ Main.java
@@ -1,22 +1,5 @@
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Scanner;
-
 class Main{
-    static int n;
-    static ArrayList<Integer> ary = new ArrayList<>();  //거리의 합이 최소가 되려면 가운데에 위치해야 한다.
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        n= sc.nextInt();
-        for (int i = 0; i < n; i++) {
-            ary.add(sc.nextInt());
-        }
-        Collections.sort(ary);
-
-        int result=0;
-        int index = (ary.size() - 1) / 2;
-        System.out.println(ary.get(index));
 
     }
-
 }
