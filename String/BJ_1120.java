e66ee158d13274a0eb0154f5d79776dd287703cd
LeeJaeHong <0915jhlee@daum.net>
2022-11-04 20:00:34 +0900

    add BJ 1120 String

--- String/BJ_1120.java
+++ String/BJ_1120.java
@@ -1,11 +1,11 @@
+package String;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.Scanner;
 import java.util.StringTokenizer;
 
-class Main {
+class BJ_1120 {
     static String A;
     static String B;
 
