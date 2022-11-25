ea65a17da7adae1ac411649ca8c612aca6a21fa6
LeeJaeHong <0915jhlee@daum.net>
2022-11-26 00:15:07 +0900

    add one String

--- String/BJ_11478.java
+++ String/BJ_11478.java
@@ -1,8 +1,10 @@
+package String;
+
 import java.util.HashSet;
 import java.util.Scanner;
 import java.util.Set;
 
-class Main{
+class BJ_11478 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         String str= sc.nextLine();
