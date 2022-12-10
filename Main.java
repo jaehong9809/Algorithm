f7301f3c649738a5cab4f71d8878afab4795f626
LeeJaeHong <0915jhlee@daum.net>
2022-12-10 23:52:12 +0900

    add one inputText

--- Main.java
+++ Main.java
@@ -1,5 +1,8 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 class Main{
     public static void main(String[] args) {
-
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     }
 }
