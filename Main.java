7699b234893571392da805ce0d5eeb734f4203ed
LeeJaeHong <0915jhlee@daum.net>
2023-05-11 22:48:24 +0900

    plus greedy 2839_2

--- Main.java
+++ Main.java
@@ -1,23 +1,5 @@
-import java.util.Scanner;
-
-class Main {
+class Main{
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
-        int cnt = 0;
-        while (n > 0) {
-            if (n % 5 == 0) {
-                n -= 5;
-            }else{
-                n -= 3;
-            }
-            cnt++;
-        }
-        if(n<0){
-            System.out.println(-1);
-        }else{
-            System.out.println(cnt);
 
-        }
     }
 }
