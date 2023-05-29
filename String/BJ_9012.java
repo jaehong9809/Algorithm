038fab9f54552fa0331b6a62601e97300206d264
LeeJaeHong <0915jhlee@daum.net>
2023-05-29 22:59:17 +0900

    plus string 9012

--- String/BJ_9012.java
+++ String/BJ_9012.java
@@ -1,7 +1,9 @@
+package String;
+
 import java.util.Scanner;
 import java.util.Stack;
 
-class Main{
+class BJ_9012 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n= sc.nextInt();
