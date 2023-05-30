1ab112f70003ae3da4cc1e032ade1869f322ee96
LeeJaeHong <0915jhlee@daum.net>
2023-05-30 23:13:59 +0900

    plus string 1181

--- String/BJ_1181.java
+++ String/BJ_1181.java
@@ -1,7 +1,12 @@
-import java.io.*;
-import java.util.*;
+package String;
 
-class Main{
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashSet;
+
+class BJ_1181 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
