9fb23d4159c346cb927af92349a73a950c774b22
LeeJaeHong <0915jhlee@daum.net>
2023-06-04 23:10:01 +0900

    plus Sorting 11004

--- Sorting/BJ_11004.java
+++ Sorting/BJ_11004.java
@@ -1,11 +1,12 @@
+package Sorting;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Collections;
-import java.util.Scanner;
 
-class Main{
+class BJ_11004 {
     static int n;
     static int k;
     static ArrayList<Integer> data = new ArrayList<>();
