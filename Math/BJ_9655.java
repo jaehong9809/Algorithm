65e0aaf5ec1aaeeca7c9336de96bbd1c0bd69fb1
LeeJaeHong <0915jhlee@daum.net>
2023-07-02 23:15:23 +0900

    plus Math 9655

--- Math/BJ_9655.java
+++ Math/BJ_9655.java
@@ -1,6 +1,8 @@
+package Math;
+
 import java.util.Scanner;
 
-class Main{
+class BJ_9655 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int n= sc.nextInt();
