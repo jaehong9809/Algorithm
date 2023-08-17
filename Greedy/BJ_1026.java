b39e16e6772abb0bea8f8b16a5c74218d8e6ec54
LeeJaeHong <0915jhlee@daum.net>
2023-08-17 23:20:44 +0900

    add Algorithm problem (Greedy-1026)

---
+++ Greedy/BJ_1026.java
@@ -0,0 +1,29 @@
+package Greedy;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Scanner;
+
+class BJ_1026 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int n= sc.nextInt();
+        int[] A=new int[n];
+        Integer[] B=new Integer[n];
+
+        for (int i = 0; i <n ; i++) {
+            A[i]=sc.nextInt();
+        }
+        for (int i = 0; i <n ; i++) {
+            B[i]=sc.nextInt();
+        }
+        Arrays.sort(A);
+        Arrays.sort(B, Collections.reverseOrder());
+        int cnt=0;
+        for (int i = 0; i < n; i++) {
+            cnt+=A[i]*B[i];
+        }
+        System.out.println(cnt);
+
+    }
+}
---
+++ Solution.java
@@ -0,0 +1,10 @@
+public class Solution {
+
+    public static void main(String[] args) {
+
+    }
+    public boolean solution(String[] phone_book) {
+        boolean answer = true;
+        return answer;
+    }
+}
