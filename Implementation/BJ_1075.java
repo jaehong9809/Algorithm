82451dc68cdb9778ee7a3a4936a123c1560f004a
LeeJaeHong <0915jhlee@daum.net>
2023-09-25 23:16:36 +0900

    solve bj-1075 Implementation

--- Implementation/BJ_1075.java
+++ Implementation/BJ_1075.java
@@ -1,8 +1,10 @@
+package Implementation;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 
-class Main{
+class BJ_1075 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int n = (Integer.parseInt(br.readLine())/100)*100;
