61d9086a9d1e291f496fc2fde28bf816d2a16431
LeeJaeHong <0915jhlee@daum.net>
2023-10-16 16:28:52 +0900

    solve HateSameNumber.java

--- DataStructures/HateSameNumber.java
+++ DataStructures/HateSameNumber.java
@@ -1,9 +1,12 @@
+package DataStructures;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.Arrays;
+import java.util.Stack;
 
-public class Solution {
+public class HateSameNumber {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String s = br.readLine();
