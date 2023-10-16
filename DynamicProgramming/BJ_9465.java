2f4a0710755e2c92ab32908ec282c8d27b3ad5c0
LeeJaeHong <0915jhlee@daum.net>
2023-10-16 16:31:47 +0900

    recommit BJ_9465.java

--- DynamicProgramming/BJ_9465.java
+++ DynamicProgramming/BJ_9465.java
@@ -1,9 +1,10 @@
+package DynamicProgramming;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.Scanner;
 
-class Main {
+class BJ_9465 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int testCase = Integer.parseInt(br.readLine());
