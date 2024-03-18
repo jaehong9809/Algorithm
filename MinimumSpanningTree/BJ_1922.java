3d422fc6ec30eb67d8246a75a7a230add71ea19e
LeeJaeHong <0915jhlee@daum.net>
2024-03-18 23:32:38 +0900

    solve: MinimumSpanningTree BJ_1922.java
    have to resolve

---
+++ MinimumSpanningTree/BJ_1922.java
@@ -0,0 +1,71 @@
+package MinimumSpanningTree;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
+class BJ_1922 {
+    static int v, e;
+    static int[] parent=new int[10001];
+    static int result=0;
+    static ArrayList<Edge> edges = new ArrayList<>();
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        v=sc.nextInt();
+        e=sc.nextInt();
+
+        for (int i = 1; i <= v; i++) {
+            parent[i]=i;
+        }
+
+        for (int i = 0; i < e; i++) {
+            int a=sc.nextInt();
+            int b=sc.nextInt();
+            int c=sc.nextInt();
+            edges.add(new Edge(c, a, b));
+        }
+        Collections.sort(edges);
+
+        for (Edge edge : edges) {
+            int cost = edge.distance;
+            int a = edge.nodeA;
+            int b = edge.nodeB;
+
+            if (findParent(a) != findParent(b)) {
+                unionParent(a, b);
+                result += cost;
+            }
+
+        }
+        System.out.println(result);
+    }
+    static int findParent(int x){
+        if(x==parent[x])return x;
+        return parent[x]=findParent(parent[x]);
+    }
+    static void unionParent(int a, int b){
+        a=findParent(a);
+        b=findParent(b);
+        if(a<b) parent[b]=a;
+        else parent[a]=b;
+    }
+
+
+    static class Edge implements Comparable<Edge>{
+        int distance;
+        int nodeA;
+        int nodeB;
+
+        public Edge(int distance, int nodeA, int nodeB) {
+            this.distance = distance;
+            this.nodeA = nodeA;
+            this.nodeB = nodeB;
+        }
+
+        @Override
+        public int compareTo(Edge o) {
+            return this.distance-o.distance;
+        }
+    }
+}
