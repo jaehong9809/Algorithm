d26b0232385d54e4aa80853b9a0d5dcb9143bc9c
LeeJaeHong <0915jhlee@daum.net>
2024-04-18 18:57:56 +0900

    solve: dfs(backtracking) Solution_targetnumber.java

---
+++ BackTracking/Solution_targetnumber.java
@@ -0,0 +1,30 @@
+package BackTracking;
+
+class Solution_targetnumber {
+
+    static int len = 0;
+    static int cnt=0;
+    static int[] nums;
+    static int tar;
+    static public int solution(int[] numbers,int target){
+        len = numbers.length;
+        nums = numbers.clone();
+        tar = target;
+        dfs(0, 0);
+        int answer=cnt;
+        return answer;
+    }
+
+    static public void dfs(int depth, int result){
+        if (depth == len) {
+            if (tar == result) {
+                cnt++;
+            }
+            return;
+        }
+
+        dfs(depth + 1, result + nums[depth]);
+
+        dfs(depth + 1, result - nums[depth]);
+    }
+}
