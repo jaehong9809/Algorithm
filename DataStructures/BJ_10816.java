663c70f19d78786ab1c2fbbebedd426f23fae13d
LeeJaeHong <0915jhlee@daum.net>
2024-05-17 23:31:24 +0900

    solve: DataStructure BJ_10816.java

--- DataStructures/BJ_10816.java
+++ DataStructures/BJ_10816.java
@@ -1,24 +1,37 @@
 package DataStructures;
 
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 import java.util.HashMap;
-import java.util.Scanner;
+import java.util.Map;
+import java.util.StringTokenizer;
 
 class BJ_10816 {
     static int n, m;
     //10816
-    public static void main(String[] args) {
-        Scanner sc=new Scanner(System.in);
-        HashMap<Integer, Integer> hashMap = new HashMap<>();
-        n= sc.nextInt();
-        for (int i = 0; i < n; i++) {
-            int tmp= sc.nextInt();
-            hashMap.put(tmp, hashMap.getOrDefault(tmp, 0)+1);
-        }
-        m= sc.nextInt();
-        for (int i = 0; i < m; i++) {
-            int tmp= sc.nextInt();
-            System.out.print(hashMap.getOrDefault(tmp, 0)+" ");
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
 
+        Map<Integer, Integer> map = new HashMap<>();
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            int a = Integer.parseInt(st.nextToken());
+            map.put(a, map.getOrDefault(a, 0)+1);
         }
+
+        int m = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < m; i++) {
+            int target = Integer.parseInt(st.nextToken());
+            if (map.containsKey(target)) {
+                sb.append(map.get(target) + " ");
+            }else{
+                sb.append("0 ");
+            }
+        }
+        System.out.println(sb);
     }
 }
